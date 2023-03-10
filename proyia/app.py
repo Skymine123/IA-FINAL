60dd8740534061da824678c6201464b41b6d2fb6
OSCAR DAVID CASTILLO SANCHEZ <osdcastillo02@gmail.com>
2023-03-10 01:42:06 -0500

    puto

--- proyia/app.py
+++ proyia/app.py
@@ -73,9 +73,72 @@ def predecir_Salario(rating, sector, ownership, job_title, job_in_headquarters, 
  
 
  modelo = joblib.load(fileDirectory+'/random_forest.joblib') 
+ return modelo.predict([prediction_input])
 
- return modelo.predict([prediction_input])[0]
+######
+
+
+def predecir_Salario1(rating, sector, ownership, job_title, job_in_headquarters, job_seniority, job_skills):
  
+ absolutepath = os.path.abspath(__file__)
+ fileDirectory = os.path.dirname(absolutepath)
+  
+ prediction_input = list()
+ print(fileDirectory)
+
+ with open(fileDirectory+"/sc_rating.pkl", "rb") as file:
+  sc_rating = pickle.load(file) 
+
+
+
+ prediction_input.append(sc_rating.transform(np.array(rating).reshape(1, -1)))
+
+
+ sector_columns = ['sector_Health Care','sector_Business Services','sector_Information Technology']
+ temp = list(map(int, np.zeros(shape=(1, len(sector_columns)))[0]))
+ for index in range(0, len(sector_columns)):
+    if sector_columns[index] == 'sector_' + sector:
+      temp[index] = 1
+      break
+ prediction_input = prediction_input + temp
+
+
+ if ownership == 'Private':
+    prediction_input.append(1)
+ else:
+    prediction_input.append(0)
+  
+
+ job_title_columns = ['job_title_data scientist', 'job_title_data analyst']
+ temp = list(map(int, np.zeros(shape=(1, len(job_title_columns)))[0]))
+ for index in range(0, len(job_title_columns)):
+    if job_title_columns[index] == 'job_title_' + job_title:
+      temp[index] = 1
+      break
+ prediction_input = prediction_input + temp
+
+
+ prediction_input.append(job_in_headquarters)
+
+
+ job_seniority_map = {'other': 0, 'jr': 1, 'sr': 2}
+ prediction_input.append(job_seniority_map[job_seniority])
+
+
+ temp = list(map(int, np.zeros(shape=(1, 4))[0]))
+ if 'excel' in job_skills:
+    temp[0] = 1
+ if 'python' in job_skills:
+    temp[1] = 1
+ if 'tableau' in job_skills:
+    temp[2] = 1
+ if 'sql' in job_skills:
+    temp[3] = 1
+ prediction_input = prediction_input + temp
+ 
+
+ modelo = joblib.load(fileDirectory+'/voting_regressor.joblib') 
+ return modelo.predict([prediction_input])[0]
  
 from flask import Flask,request
 #from flask_mandrill import Mandrill
@@ -114,9 +177,12 @@ def obtener_datos():
 
    print(job_skills)   
    salary=predecir_Salario(rating,sector,ownership,job_title,job_in_headquarters,job_seniority,job_skills)
-   texto='Salario Estimado (rango): {}(USD) a {}(USD) por año.'.format((int(salary*1000)-9000), (int(salary*1000)+9000))
-   print(texto)
-   return json.dumps({"message":texto})
+   salary1=predecir_Salario1(rating,sector,ownership,job_title,job_in_headquarters,job_seniority,job_skills)
+   texto='Salario Estimado FOREST REGRESSOR (rango): {}(USD) a {}(USD) por año.'.format((int(salary*1000)-9000), (int(salary*1000)+9000))
+   texto1='Salario Estimado VOTING REGRESSOR (rango): {}(USD) a {}(USD) por año.'.format((int(salary1*1000)-9000), (int(salary1*1000)+9000))
+   texto2=texto+"\n"+texto1
+   print(texto2)
+   return json.dumps({"message":texto2})
 
 
 
